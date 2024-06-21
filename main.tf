2a130f28024aa7f99ddde650539bb5389d8ff44c
root <root@cicd.us-central1-c.c.mineral-battery-424603-v8.internal>
2024-06-21 06:14:36 +0000

    update ami to ubuntu

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_key_pair" "generated_key" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-08a0d1e16fc3f61ea"  # Specify an appropriate AMI ID
+    ami           = "ami-06aa3f7caf3a30282"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
     key_name      = aws_key_pair.generated_key.key_name
 }
